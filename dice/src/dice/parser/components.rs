fb52f7a2174775e380507c1940762ae609406b2c
Agustín Guzmán <agustin.guzman.v@proton.me>
2026-02-23 15:00:47 -0300

    refactor

--- dice/src/dice/parser/components.rs
+++ dice/src/dice/parser/components.rs
@@ -5,6 +5,61 @@ use std::str::FromStr;
 
 pub struct Components(Vec<Component>);
 
+impl Components {
+    fn parse_component(input: &str) -> Result<Option<(Component, &str)>, ComponentError> {
+        if let Some(res) = Self::parse_advantage(input) {
+            return Ok(Some(res));
+        }
+
+        if let Some(res) = Self::parse_disadvantage(input) {
+            return Ok(Some(res));
+        }
+
+        if let Some(res) = Self::parse_modifier(input)? {
+            return Ok(Some(res));
+        }
+
+        Ok(None)
+    }
+
+    fn parse_advantage(input: &str) -> Option<(Component, &str)> {
+        if let Some(rest) = input.strip_prefix(&Component::Advantage.to_string()) {
+            return Some((Component::Advantage, rest));
+        }
+
+        None
+    }
+
+    fn parse_disadvantage(input: &str) -> Option<(Component, &str)> {
+        if let Some(rest) = input.strip_prefix(&Component::Disadvantage.to_string()) {
+            return Some((Component::Disadvantage, rest));
+        }
+
+        None
+    }
+
+    fn parse_modifier(input: &str) -> Result<Option<(Component, &str)>, ComponentError> {
+        if !(input.starts_with('+') || input.starts_with('-')) {
+            return Ok(None);
+        }
+
+        let digits = input[1..]
+            .chars()
+            .take_while(|c| c.is_ascii_digit())
+            .count();
+
+        if digits == 0 {
+            return Err(ComponentError::InvalidModifier);
+        }
+
+        let token = &input[..1 + digits];
+        let component = token.parse()?;
+        let rest = &input[1 + digits..];
+
+        Ok(Some((component, rest)))
+    }
+}
+
 impl FromStr for Components {
     type Err = ComponentError;
 
@@ -12,38 +67,15 @@ impl FromStr for Components {
         let mut components = Vec::new();
 
         while !input.is_empty() {
-            // advantage
-            if let Some(rest) = input.strip_prefix("adv") {
-                components.push("adv".parse()?);
-                input = rest;
-                continue;
-            }
-
-            // disadvantage
-            if let Some(rest) = input.strip_prefix("dis") {
-                components.push("dis".parse()?);
-                input = rest;
-                continue;
-            }
-
-            // modifier (+/-int)
-            if input.starts_with('+') || input.starts_with('-') {
-                let digits = input[1..]
-                    .chars()
-                    .take_while(|c| c.is_ascii_digit())
-                    .count();
-
-                if digits == 0 {
-                    return Err(ComponentError::InvalidModifier.into());
+            match Self::parse_component(input)? {
+                Some((component, rest)) => {
+                    components.push(component);
+                    input = rest;
                 }
-
-                let token = &input[..1 + digits];
-                components.push(token.parse()?);
-                input = &input[1 + digits..];
-                continue;
+                None => {
+                    return Err(ComponentError::InvalidComponent(input.to_string()));
+                }
             }
-
-            return Err(ComponentError::InvalidComponent(input.to_string()).into());
         }
 
         Ok(Components(components))
